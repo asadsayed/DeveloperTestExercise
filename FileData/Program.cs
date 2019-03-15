dd7b445c0da868f6f092a0df428b7f723d7f5e41
Asad Sayed <asadsayed26@gmail.com>
2019-03-15 01:05:17 +0000

    Add files via upload

---
+++ FileData/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is the functionality you would like to perform? ");
+            string func = Console.ReadLine();
+            Console.WriteLine("\nWhat is the file name? ");
+            string name = Console.ReadLine();
+            if (func == "–v" || func == "--v" || func == "/v" || func ==  "--version" )
+            {
+                string version;
+                FileDetails obj = new FileDetails();
+                version = obj.Version(name);
+                Console.WriteLine($"The version of the file {name} is {version}");
+            }
+            else if(func == "–s" || func == "--s" || func == "/s" || func == "--size")
+            {
+                int size;
+                FileDetails obj = new FileDetails();
+                size = obj.Size(name);
+                Console.WriteLine($"The size of the file {name} is {size}");
+            }
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
